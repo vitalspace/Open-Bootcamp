6e4059f2e6de58ae6d3c8ed4838993c28bcc671d
vitalspace <myglobalsounds@gmail.com>
2022-09-30 22:42:34 +0000

    Ejercicio 2

---
+++ js/hello/yo.js
@@ -0,0 +1,9 @@
+const birthday = new Date(1994 -1, 12, 04)
+const libro = {
+    titulo: "El Caballero De La Armadura Oxidada",
+    autor: "Robert Fisher",
+    fecha: "3 noviembre 1997",
+    url: "https://www.amazon.com.mx/Caballero-Armadura-Oxidada-Cdno-Actividades/dp/849777230X"
+};
+const yo = [ "vital", 27, true, birthday, libro ];
+console.log(birthday)
